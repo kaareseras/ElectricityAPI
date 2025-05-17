5ccb01db4c8f9e717f963235f22d69d12489fb34
kaareseras <kaare@seras.dk>
2025-05-17 19:18:36 +0000

    changed user and device model

--- src/fastapi_app/models/device.py
+++ src/fastapi_app/models/device.py
@@ -1,14 +1,18 @@
 # Description: Tax model for database table creation.
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, func
+from sqlalchemy import DateTime, ForeignKey, Integer, String, func
+from sqlalchemy.orm import mapped_column, relationship
 
 from src.fastapi_app.config.database import Base
 
 
 class Device(Base):
     __tablename__ = "device"
-    uuid = Column(String(100), primary_key=True, nullable=False)
-    chargeowner_id = Column(Integer, ForeignKey("chargeowner.id"), nullable=True)
-    PriceArea = Column(String(15), nullable=True)
-    Config = Column(String(1000), nullable=True)
-    last_activity = Column(DateTime, nullable=True)
-    created_at = Column(DateTime, nullable=False, server_default=func.now())
+    uuid = mapped_column(String(100), primary_key=True, nullable=False)
+    user_id = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    chargeowner_id = mapped_column(Integer, ForeignKey("chargeowner.id"), nullable=True)
+    PriceArea = mapped_column(String(15), nullable=True)
+    Config = mapped_column(String(1000), nullable=True)
+    last_activity = mapped_column(DateTime, nullable=True)
+    created_at = mapped_column(DateTime, nullable=False, server_default=func.now())
+
+    user = relationship("User", back_populates="devices")
