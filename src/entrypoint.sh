7ba6c0fddfadae8ecc828196da1aca86faf0b187
kaareseras <kaare@seras.dk>
2025-06-03 07:29:35 +0000

    Add forwarded-allow-ips option to gunicorn command in entrypoint script

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,4 +3,4 @@ set -e
 python3 -m pip install --upgrade pip
 python3 -m pip install -e src
 python3 -m alembic upgrade head
-python3 -m gunicorn fastapi_app.app:app -c src/gunicorn.conf.py
+python3 -m gunicorn fastapi_app.app:app -c src/gunicorn.conf.py --forwarded-allow-ips '*'
