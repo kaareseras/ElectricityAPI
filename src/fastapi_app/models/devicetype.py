d3e4af9a95f43d4f7c9aed12b4f6b417dcdb073a
kaareseras <kaare@seras.dk>
2025-07-05 17:40:20 +0000

    Update models

--- src/fastapi_app/models/devicetype.py
+++ src/fastapi_app/models/devicetype.py
@@ -1,5 +1,5 @@
 # Description: Tax model for database table creation.
-from sqlalchemy import Date, Integer, String
+from sqlalchemy import Boolean, Date, Integer, String
 from sqlalchemy.orm import mapped_column, relationship
 
 from src.fastapi_app.config.database import Base
@@ -16,3 +16,34 @@ class DeviceType(Base):
 
 
 DeviceType.devices = relationship("Device", back_populates="devicetype")
+
+
+class Firmware(Base):
+    __tablename__ = "firmware"
+
+    id = mapped_column(Integer, primary_key=True, autoincrement=True)
+    devicetype_id = mapped_column(Integer, nullable=False)
+
+    version = mapped_column(String(50), nullable=True)
+    filename = mapped_column(String(50), nullable=True)
+    repo_url = mapped_column(String(50), nullable=True)
+    is_active = mapped_column(Boolean, nullable=True)
+    created_at = mapped_column(Date, nullable=True)
+
+
+Firmware.devicetypes = relationship("DeviceType", back_populates="firmwares")
+
+
+class Hardware(Base):
+    __tablename__ = "hardware"
+
+    id = mapped_column(Integer, primary_key=True, autoincrement=True)
+    devicetype_id = mapped_column(Integer, nullable=False)
+    version = mapped_column(String(50), nullable=True)
+    name = mapped_column(String(100), nullable=False)
+    description = mapped_column(String(500), nullable=False)
+    created_at = mapped_column(Date, nullable=True)
+    is_active = mapped_column(Boolean, nullable=True)
+
+
+Hardware.devicetypes = relationship("DeviceType", back_populates="hardwares")
